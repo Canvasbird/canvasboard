be19882855fcd18c69be9d9123ac55a8f8dbe90b
goliakshay357 <goliakshay357@gmail.com>
2020-08-01 03:48:49 +0530

    Made static home page and added login and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BoardComponent } from './components/board/board.component';
 import { FileExplorerComponent } from './components/file-explorer/file-explorer.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BoardComponent,
-    FileExplorerComponent
+    FileExplorerComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
