f0c87d93952aec842630fddbe2cbad4cb3a9d350
Goli Akshay <goliakshay357@gmail.com>
2020-09-27 22:13:52 +0530

    Updated Dashboard UI

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:Router) { }
+
+  data:any;
 
   ngOnInit() {
+    this.data = [
+      {
+        "imageURL": "https://i.gifer.com/XXM2.gif",
+        "title": "Chapter 1",
+        "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing eliteius.",
+        "id":1,
+        "filters": ["one","two","three"]
+
+      },
+      {
+        "imageURL": "https://img.wallpapersafari.com/tablet/1536/2048/2/20/JGTEAp.jpg",
+        "title": "Chapter 2",
+        "subtitle": "Lorem Lorem ipsum dolor sit amet. ipsum dolor sit amet consectetur adipisicing eliteius.",
+        "id":2,
+        "filters": ["one","two","three"]
+
+      },
+    ]
   }
 
+  navigateToFiles(e,item) {
+    console.log("Working",item);
+    this.route.navigate(['/files']);
+  }
 }
