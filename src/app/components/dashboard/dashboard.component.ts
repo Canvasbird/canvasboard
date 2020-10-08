cbe9817401de3dbedeae41b8e24eded118baa27d
Voon <jawen.voon@zuhlke.com>
2020-10-08 14:21:13 +0800

    Add dashboard interface file

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { DashboardData } from 'src/interfaces/dashboard';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,33 +9,33 @@ import {Router} from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private route:Router) { }
+  constructor(private route: Router) { }
 
-  data:any;
+  data: DashboardData[];
 
   ngOnInit() {
     this.data = [
       {
-        "imageURL": "https://i.gifer.com/XXM2.gif",
-        "title": "Chapter 1",
-        "subtitle": "Lorem ipsum dolor sit amet consectetur adipisicing eliteius.",
-        "id":1,
-        "filters": ["one","two","three"]
+        imageUrl: 'https://i.gifer.com/XXM2.gif',
+        title: 'Chapter 1',
+        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing eliteius.',
+        id: 1,
+        filters: ['one', 'two', 'three']
 
       },
       {
-        "imageURL": "https://img.wallpapersafari.com/tablet/1536/2048/2/20/JGTEAp.jpg",
-        "title": "Chapter 2",
-        "subtitle": "Lorem Lorem ipsum dolor sit amet. ipsum dolor sit amet consectetur adipisicing eliteius.",
-        "id":2,
-        "filters": ["one","two","three"]
+        imageUrl: 'https://img.wallpapersafari.com/tablet/1536/2048/2/20/JGTEAp.jpg',
+        title: 'Chapter 2',
+        subtitle: 'Lorem Lorem ipsum dolor sit amet. ipsum dolor sit amet consectetur adipisicing eliteius.',
+        id: 2,
+        filters: ['one', 'two', 'three']
 
       },
-    ]
+    ];
   }
 
-  navigateToFiles(e,item) {
-    console.log("Working",item);
+  navigateToFiles(e, item) {
+    console.log('Working', item);
     this.route.navigate(['/files']);
   }
 }
---
+++ src/interfaces/dashboard.ts
@@ -0,0 +1,7 @@
+export interface DashboardData {
+    imageUrl: string;
+    title: string;
+    subtitle: string;
+    id: number;
+    filters: string[];
+}
