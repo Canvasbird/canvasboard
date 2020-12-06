5fe352386cab5acaa532c7015fb7fab8bd44e225
kingavatar <saikiranreddy06@gmail.com>
2020-12-07 01:32:36 +0530

    Add Save button and Print Json File

--- src/app/components/new-board/new-board-card.ts
+++ src/app/components/new-board/new-board-card.ts
@@ -1,8 +1,9 @@
+export type PluginType = 'editor' | 'board' | 'youtube' | 'fileUpload';
 export interface NewBoardCardInterface {
     cardID: number;
     oldPosition: any;
     newPosition: any;
-    pluginType: string;
+    pluginType: PluginType;
     saveData: any;
 }
 
@@ -10,14 +11,20 @@ export class NewBoardCard implements NewBoardCardInterface {
     cardID: number;
     oldPosition: any;
     newPosition: any;
-    pluginType: string;
+    pluginType: PluginType;
     saveData: any;
 
     public getpluginType(): any {
         return this.pluginType;
     }
-    public setpluginType(value: any) {
+    public setpluginType(value: PluginType) {
         this.pluginType = value;
+    }
+    public setData(value: any) {
+        this.saveData = value;
+    }
+    public getData(): any {
+        return this.saveData;
     }
     constructor(cardID: number, oldPosition: any, newPosition: any) {
         this.cardID = cardID;
