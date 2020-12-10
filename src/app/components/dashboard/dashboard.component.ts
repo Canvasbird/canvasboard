2b43374fb389680eefcc073caeafa081d3ce750f
Dhamodhar <56181018+Dhamodhar-DDR@users.noreply.github.com>
2020-12-11 04:56:55 +0530

    Added edit name functionality to folder name (#237)
    * Added edit name functionality
    
    * Added edit functionality to folder names
    
    Co-authored-by: Dhamodhar Reddy <ddrdhamodharreddy@gmail.com>

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -53,7 +53,47 @@ export class DashboardComponent implements OnInit {
           <button class="btn btn-outline-danger">Sure?</button>
         </div>
       </div>
-      <h5 class="folder-title"><strong>${obj.folder_name}</strong></h5>
+      <span id=edit-name-input-${obj._id} style="display:none">
+        <input class="folder-title" style="border:none; outline: none; border-radius:10em"id=new-name-text-${obj._id}
+        value="${obj.folder_name}" type="text">
+        <button style="border-style:none; outline: none; background-color:transparent" id=button-edit-name-ok-${obj._id}>
+          <svg  width="2em" height="2em"  xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 550 550">
+            <circle style="fill:#32BA7C;" cx="253.6" cy="253.6" r="253.6"/>
+            <path style="fill:#0AA06E;" d="M188.8,368l130.4,130.4c108-28.8,188-127.2,188-244.8c0-2.4,0-4.8,0-7.2L404.8,152L188.8,368z"/>
+              <path style="fill:#FFFFFF;" d="M260,310.4c11.2,11.2,11.2,30.4,0,41.6l-23.2,23.2c-11.2,11.2-30.4,11.2-41.6,0L93.6,272.8
+                c-11.2-11.2-11.2-30.4,0-41.6l23.2-23.2c11.2-11.2,30.4-11.2,41.6,0L260,310.4z"/>
+              <path style="fill:#FFFFFF;" d="M348.8,133.6c11.2-11.2,30.4-11.2,41.6,0l23.2,23.2c11.2,11.2,11.2,30.4,0,41.6l-176,175.2
+                c-11.2,11.2-30.4,11.2-41.6,0l-23.2-23.2c-11.2-11.2-11.2-30.4,0-41.6L348.8,133.6z"/>
+          </svg>
+        </button>
+        <button style="border-style:none; outline: none; background-color:transparent" id=button-edit-name-no-${obj._id}>
+          <svg  width="2em" height="2em"  xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 500 500">
+            <circle style="fill:#E24C4B;" cx="227.556" cy="227.556" r="227.556"/>
+            <path style="fill:#D1403F;" d="M455.111,227.556c0,125.156-102.4,227.556-227.556,227.556c-72.533,0-136.533-32.711-177.778-85.333
+              c38.4,31.289,88.178,49.778,142.222,49.778c125.156,0,227.556-102.4,227.556-227.556c0-54.044-18.489-103.822-49.778-142.222
+              C422.4,91.022,455.111,155.022,455.111,227.556z"/>
+            <path style="fill:#FFFFFF;" d="M331.378,331.378c-8.533,8.533-22.756,8.533-31.289,0l-72.533-72.533l-72.533,72.533
+              c-8.533,8.533-22.756,8.533-31.289,0c-8.533-8.533-8.533-22.756,0-31.289l72.533-72.533l-72.533-72.533
+              c-8.533-8.533-8.533-22.756,0-31.289c8.533-8.533,22.756-8.533,31.289,0l72.533,72.533l72.533-72.533
+              c8.533-8.533,22.756-8.533,31.289,0c8.533,8.533,8.533,22.756,0,31.289l-72.533,72.533l72.533,72.533
+              C339.911,308.622,339.911,322.844,331.378,331.378z"/>
+          </svg>
+        </button>
+      </span>
+      <h5 class="folder-title" id=folder-name-${obj._id}>
+        <strong id=name-display-${obj._id}>${obj.folder_name}</strong>
+        <button style="border-style:none; color:rgb(99, 64, 88); outline: none; margin-left:10px;background-color:transparent"
+        id=button-edit-name-${obj._id}>
+        <svg  width="0.8em" height="0.8em" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 388.947 388.947">
+        <polygon points="0,303.947 0,383.947 80,383.947 316.053,147.893 236.053,67.893 			"/>
+          <path d="M377.707,56.053L327.893,6.24c-8.32-8.32-21.867-8.32-30.187,0l-39.04,39.04l80,80l39.04-39.04
+            C386.027,77.92,386.027,64.373,377.707,56.053z"/>
+        </svg>
+        </button>
+      </h5>
       <!-- <p class="folder-discription">Lorem ipsum dolor sit amet.</p> -->
       <button class="btn btn-dark" id=button-${obj._id}>Enter</button>
     </div>
@@ -63,7 +103,39 @@ export class DashboardComponent implements OnInit {
       $(`#button-${obj._id}`).click( () => {
         this.route.navigate([`/folder/${obj._id}`]);
       });
+    // Click action to edit the folder_name
+      $(`#button-edit-name-${obj._id}`).click( () => {
+        const folderName = document.getElementById(`folder-name-${obj._id}`);
+        const editText = document.getElementById(`edit-name-input-${obj._id}`);
+        if (editText.style.display === 'block') {
+          editText.style.display = 'none';
+          folderName.style.display = 'block';
 
+        } else {
+          editText.style.display = 'block';
+          folderName.style.display = 'none';
+        }
+      });
+    // Click action to close the edit input
+      $(`#button-edit-name-no-${obj._id}`).click( () => {
+        const folderName = document.getElementById(`folder-name-${obj._id}`);
+        const editText = document.getElementById(`edit-name-input-${obj._id}`);
+        if (editText.style.display === 'block') {
+          editText.style.display = 'none';
+          folderName.style.display = 'block';
+        }
+      });
+    // Click action to save the new edited name
+      $(`#button-edit-name-ok-${obj._id}`).click( () => {
+        const newName = (document.getElementById(`new-name-text-${obj._id}`) as HTMLInputElement).value;
+        const folderName = document.getElementById(`folder-name-${obj._id}`);
+        const editText = document.getElementById(`edit-name-input-${obj._id}`);
+        this.renameFolder(obj, newName);
+        if (editText.style.display === 'block') {
+          editText.style.display = 'none';
+          folderName.style.display = 'block';
+        }
+      });
     // Open delete popup
       $(`#delete-${obj._id}`).click( () => {
       const popup = document.getElementById(`delete-sure-${obj._id}`);
@@ -132,9 +204,49 @@ export class DashboardComponent implements OnInit {
         <button class="btn btn-outline-danger">Sure?</button>
       </div>
     </div>
-    <h5 class="folder-title"><strong>${obj.folder_name}</strong></h5>
-    <!-- <p class="folder-discription">Lorem ipsum dolor sit amet.</p> -->
-    <button class="btn btn-dark" id=button-${obj._id}>Enter</button>
+    <span id=edit-name-input-${obj._id} style="display:none">
+        <input class="folder-title" style="border:none; outline: none; border-radius:10em"id=new-name-text-${obj._id}
+        value="${obj.folder_name}" type="text">
+        <button style="border-style:none; outline: none; background-color:transparent" id=button-edit-name-ok-${obj._id}>
+          <svg  width="2em" height="2em"  xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 550 550">
+            <circle style="fill:#32BA7C;" cx="253.6" cy="253.6" r="253.6"/>
+            <path style="fill:#0AA06E;" d="M188.8,368l130.4,130.4c108-28.8,188-127.2,188-244.8c0-2.4,0-4.8,0-7.2L404.8,152L188.8,368z"/>
+              <path style="fill:#FFFFFF;" d="M260,310.4c11.2,11.2,11.2,30.4,0,41.6l-23.2,23.2c-11.2,11.2-30.4,11.2-41.6,0L93.6,272.8
+                c-11.2-11.2-11.2-30.4,0-41.6l23.2-23.2c11.2-11.2,30.4-11.2,41.6,0L260,310.4z"/>
+              <path style="fill:#FFFFFF;" d="M348.8,133.6c11.2-11.2,30.4-11.2,41.6,0l23.2,23.2c11.2,11.2,11.2,30.4,0,41.6l-176,175.2
+                c-11.2,11.2-30.4,11.2-41.6,0l-23.2-23.2c-11.2-11.2-11.2-30.4,0-41.6L348.8,133.6z"/>
+          </svg>
+        </button>
+        <button style="border-style:none; outline: none; background-color:transparent" id=button-edit-name-no-${obj._id}>
+          <svg  width="2em" height="2em"  xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 500 500">
+            <circle style="fill:#E24C4B;" cx="227.556" cy="227.556" r="227.556"/>
+            <path style="fill:#D1403F;" d="M455.111,227.556c0,125.156-102.4,227.556-227.556,227.556c-72.533,0-136.533-32.711-177.778-85.333
+              c38.4,31.289,88.178,49.778,142.222,49.778c125.156,0,227.556-102.4,227.556-227.556c0-54.044-18.489-103.822-49.778-142.222
+              C422.4,91.022,455.111,155.022,455.111,227.556z"/>
+            <path style="fill:#FFFFFF;" d="M331.378,331.378c-8.533,8.533-22.756,8.533-31.289,0l-72.533-72.533l-72.533,72.533
+              c-8.533,8.533-22.756,8.533-31.289,0c-8.533-8.533-8.533-22.756,0-31.289l72.533-72.533l-72.533-72.533
+              c-8.533-8.533-8.533-22.756,0-31.289c8.533-8.533,22.756-8.533,31.289,0l72.533,72.533l72.533-72.533
+              c8.533-8.533,22.756-8.533,31.289,0c8.533,8.533,8.533,22.756,0,31.289l-72.533,72.533l72.533,72.533
+              C339.911,308.622,339.911,322.844,331.378,331.378z"/>
+          </svg>
+        </button>
+      </span>
+      <h5 class="folder-title" id=folder-name-${obj._id}>
+        <strong id=name-display-${obj._id}>${obj.folder_name}</strong>
+        <button style="border-style:none; color:rgb(99, 64, 88); outline: none; margin-left:10px;background-color:transparent"
+        id=button-edit-name-${obj._id}>
+        <svg  width="0.8em" height="0.8em" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 388.947 388.947">
+        <polygon points="0,303.947 0,383.947 80,383.947 316.053,147.893 236.053,67.893 			"/>
+          <path d="M377.707,56.053L327.893,6.24c-8.32-8.32-21.867-8.32-30.187,0l-39.04,39.04l80,80l39.04-39.04
+            C386.027,77.92,386.027,64.373,377.707,56.053z"/>
+        </svg>
+        </button>
+      </h5>
+  <!-- <p class="folder-discription">Lorem ipsum dolor sit amet.</p> -->
+  <button class="btn btn-dark" id=button-${obj._id}>Enter</button>
   </div>
     `);
 
@@ -142,8 +254,40 @@ export class DashboardComponent implements OnInit {
     $(`#button-${obj._id}`).click( () => {
         this.route.navigate([`/folder/${obj._id}`]);
     });
+    // Click action to edit the folder name.
+    $(`#button-edit-name-${obj._id}`).click( () => {
+      const folderName = document.getElementById(`folder-name-${obj._id}`);
+      const editText = document.getElementById(`edit-name-input-${obj._id}`);
+      if (editText.style.display === 'block') {
+        editText.style.display = 'none';
+        folderName.style.display = 'block';
 
-    // Open delete popup
+      } else {
+        editText.style.display = 'block';
+        folderName.style.display = 'none';
+      }
+    });
+
+    // Click action to close the edited input
+    $(`#button-edit-name-no-${obj._id}`).click( () => {
+      const folderName = document.getElementById(`folder-name-${obj._id}`);
+      const editText = document.getElementById(`edit-name-input-${obj._id}`);
+      if (editText.style.display === 'block') {
+        editText.style.display = 'none';
+        folderName.style.display = 'block';
+      }
+    });
+    // Click action to save the new edited name
+    $(`#button-edit-name-ok-${obj._id}`).click( () => {
+      const newName = (document.getElementById(`new-name-text-${obj._id}`) as HTMLInputElement).value;
+      const folderName = document.getElementById(`folder-name-${obj._id}`);
+      const editText = document.getElementById(`edit-name-input-${obj._id}`);
+      this.renameFolder(obj, newName);
+      if (editText.style.display === 'block') {
+        editText.style.display = 'none';
+        folderName.style.display = 'block';
+      }
+    });    // Open delete popup
     $(`#delete-${obj._id}`).click( () => {
       const popup = document.getElementById(`delete-sure-${obj._id}`);
       if (popup.style.display === 'block') {
@@ -185,6 +329,26 @@ export class DashboardComponent implements OnInit {
       $('#newCard').modal('hide');
     } else {
       // Error Label in popup
+      document.getElementById('error-label').style.display = 'block';
+    }
+  }
+
+  async renameFolder(obj, newName) {
+    const body = {
+      folder_name: newName,
+      folder_title: obj.folder_title,
+      folder_id: obj._id,
+      folder_tag: obj.folder_tag,
+      folder_color: 'folder_color',
+      is_modified: true,
+      is_pinned: false
+    };
+    const response = await this.apiService.renameFolder(body);
+    if (response.success) {
+      // Change the previous name of the folder in the display.
+      const folderNameHeading = document.getElementById(`name-display-${obj._id}`);
+      folderNameHeading.innerText = newName;
+    } else {
       document.getElementById('error-label').style.display = 'block';
     }
   }
