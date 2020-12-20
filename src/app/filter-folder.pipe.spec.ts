b1694d5be58e3a1534c73833b69d5f8ffceecda9
AbhiDroid <abhinabroy2001@gmail.com>
2020-12-20 20:58:07 +0530

    deleted unnecessary files

--- src/app/filter-folder.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterFolderPipe } from './filter-folder.pipe';
-
-describe('FilterFolderPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterFolderPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
