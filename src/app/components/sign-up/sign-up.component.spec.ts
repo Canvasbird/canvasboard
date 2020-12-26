1e3fb8f15f8e73a864645784a9f1c20db091bf71
AbhiDroid <abhinabroy2001@gmail.com>
2020-12-26 23:57:48 +0530

    fix push errors

--- src/app/components/sign-up/sign-up.component.spec.ts
+++ src/app/components/sign-up/sign-up.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUpComponent } from './sign-up.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatPasswordStrengthModule } from '@angular-material-extensions/password-strength';
+
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
@@ -13,7 +15,7 @@ describe('SignUpComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SignUpComponent ],
-      imports: [ RouterTestingModule, FormsModule, HttpClientTestingModule ]
+      imports: [ RouterTestingModule, FormsModule, HttpClientTestingModule, MatPasswordStrengthModule, BrowserAnimationsModule]
     })
     .compileComponents();
   }));
