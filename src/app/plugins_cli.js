ef80e7af00b33a7f27db952dc691c6b687316ba8
cache --timeout=400 <spurvaj@gmail.com>
2021-08-30 21:44:27 +0530

    removed unnecessary files

--- src/app/plugins_cli.js
+++ src/app/plugins_cli.js
@@ -3,15 +3,15 @@ var path = require('path');
 var inquirer = require('inquirer');
 
 DESTINATION = path.join(__dirname,'plugins/')
-function generateTS(name,description){
+generateTS(fname,description){
     var content = "// Description : "+description;
     fs.readdir(DESTINATION,(err,files)=>{
       if(err){
         return console.log("Destination undefined");
       }
       if(!files.includes(name+'.ts')){
-        var filename = path.join(DESTINATION,name+'.ts');
-        fs.writeFile(filename, content, function (err) {
+        var filename = path.join(DESTINATION,fname+'.ts');
+        fs.writeFile(filename, content,function (err) {
             if (err) throw err;
             console.log('Saved!');
           });
