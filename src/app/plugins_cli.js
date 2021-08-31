fd70d9ffbdf2c9a649e6c7ee0e92cb97ed3bf20f
S Purvaj <32098569+SEETHAMRAJU@users.noreply.github.com>
2021-08-31 12:20:09 +0530

    Added automatic generation of plugin files after few changes according to requirements. (#411)
    * optimized docker file
    
    * Update Dockerfile
    
    * automatic generation of ts file for plugins
    
    * removed unnecessary files
    
    * changed code to typescript
    
    * updated as per requirements
    
    * Delete package-lock.json
    
    Co-authored-by: Goli Akshay Sujith <32712438+goliakshay357@users.noreply.github.com>

---
+++ src/app/plugins_cli.js
@@ -0,0 +1,51 @@
+let fs = require('fs');
+let path = require('path');
+let inquirer = require('inquirer');
+
+let DESTINATION = path.join(__dirname,'plugins/')
+
+
+let generateTS = (fname, description)=>{
+    let content = `
+    // description: ${description}
+    import { PluginComponent } from 'src/interfaces/plugin-component';
+    declare var $: any;
+    export class ${fname} implements PluginComponent{
+    
+    }`;
+    fs.readdir(DESTINATION,(err,files)=>{
+      if(err){
+        return console.log("Destination undefined");
+      }
+      if(!files.includes(fname+'.ts')){
+        let filename = path.join(DESTINATION,fname+'.ts');
+        fs.writeFile(filename, content,(err)=> {
+            if (err) throw err;
+            console.log('File created in ',DESTINATION);
+          });
+      }
+      else{
+        console.log('File already exists !!');
+      }
+    });
+}
+
+const questions = [
+  {
+    type: 'input',
+    name: 'filename',
+    message: 'Plugin filename: ',
+    default: 'Plugin.ts',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Description:  ',
+    default: 'No Description',
+  },
+]
+inquirer.prompt(questions).then((ans)=>{
+  generateTS(ans.filename,ans.description);
+}).catch((err)=>{
+  console.log(err);
+});
