dbbb26831544e052bdb36aedb2557476ea15cda6
cache --timeout=400 <spurvaj@gmail.com>
2021-08-31 12:36:47 +0530

    updated as per requirements with package-lock.json

--- src/app/plugins/Plugin1.ts
+++
@@ -1,7 +0,0 @@
-
-    // description: Description 1
-    import { PluginComponent } from 'src/interfaces/plugin-component';
-    declare var $: any;
-    export class Plugin1 implements PluginComponent{
-    
-    }
