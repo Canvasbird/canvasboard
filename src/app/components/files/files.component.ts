9df36090bbf03062a25f0de5ecf52212b3049105
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2021-10-04 00:58:50 -0400

    Trim outer space for update file name

--- src/app/components/files/files.component.ts
+++ src/app/components/files/files.component.ts
@@ -192,7 +192,7 @@ export class FilesComponent implements OnInit {
     });
     // Click action to save the new edited name
     $(`#button-edit-name-ok-${data._id}`).click(() => {
-      const newName = (document.getElementById(`new-name-text-${data._id}`) as HTMLInputElement).value;
+      const newName = (document.getElementById(`new-name-text-${data._id}`) as HTMLInputElement).value.trim();
       const fileName = document.getElementById(`file-name-${data._id}`);
       const editText = document.getElementById(`edit-name-input-${data._id}`);
       const editButton = document.getElementById(`button-edit-name-${data._id}`);
