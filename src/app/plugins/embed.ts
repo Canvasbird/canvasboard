6bb2685c7224c539c7137679444f82777982980e
Saikiran Reddy <31726036+kingavatar@users.noreply.github.com>
2021-10-07 14:51:29 +0530

    Fuse Search externals (#447)

--- src/app/plugins/embed.ts
+++ src/app/plugins/embed.ts
@@ -36,6 +36,7 @@ export class AddEmbedComponent implements PluginComponent{
     <!-- Embed iframe -->
     <embed src="${url}" width="100%" height="600" frameborder="0" allowfullscreen/>
     `);
+    
     }
   }
 
