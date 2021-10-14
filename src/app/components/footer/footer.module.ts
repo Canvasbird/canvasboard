acc9a525104ef93a28ade945c3861ba2c3199394
Akula Sai Sridhar <58875230+SaiSridhar783@users.noreply.github.com>
2021-10-14 22:10:23 +0530

    Fix Footer links not navigating (#458)

--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer.component';
+import { RouterModule } from "@angular/router"
 
 @NgModule({
     declarations: [FooterComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [FooterComponent],
 })
 export class FooterModule {}
