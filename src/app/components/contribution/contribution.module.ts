a3b3d5e0d1c98a5e6964f58d7635b98c89aa708a
nidhibudhraja <nidhi.budhraja96@gmail.com>
2021-10-16 22:56:46 +0530

    updated footer module

--- src/app/components/contribution/contribution.module.ts
+++ src/app/components/contribution/contribution.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ContributionRoutingModule } from './contribution-routing.module';
 import { ContributionComponent } from './contribution.component';
+import { FooterModule } from '../footer/footer.module';
 
 @NgModule({
     declarations: [ContributionComponent],
-    imports: [CommonModule, ContributionRoutingModule],
+    imports: [CommonModule, ContributionRoutingModule, FooterModule],
 })
 export class ContributionModule {}
