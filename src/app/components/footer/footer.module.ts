a67cf1f9e4bc02261f871c92b5afc7dcd932f8f9
akshay <goliakshay357@gmail.com>
2021-12-05 13:43:41 +0530

    lint fix

--- src/app/components/footer/footer.module.ts
+++ src/app/components/footer/footer.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer.component';
-import { RouterModule } from "@angular/router"
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [FooterComponent],
