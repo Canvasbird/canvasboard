36e5e4496cbe2bc830206d1e3330776103c2fe58
Wrrikk <86593125+Wrrikk@users.noreply.github.com>
2022-11-11 10:43:58 +0530

    Create  spinner.service.ts

---
+++ src/app/components/spinner/spinner.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpinnerService {
+
+  private count = 0;
+  private spinner$ = new BehaviorSubject<string>('');
+
+  constructor() { }
+
+  getSpinnerObserver(): Observable<string> {
+    return this.spinner$.asObservable();
+  }
+
+  requestStarted() {
+    if (++this.count === 1) {
+      this.spinner$.next('start');
+    }
+  }
+
+  requestEnded() {
+    if (this.count === 0 || --this.count === 0) {
+      this.spinner$.next('stop');
+    }
+  }
+
+  resetSpinner() {
+    this.count = 0;
+    this.spinner$.next('stop');
+  }
+}
