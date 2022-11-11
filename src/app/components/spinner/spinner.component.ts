b5702324e6ceeff6a2e17336ab5ff58fc7046334
Wrrikk <86593125+Wrrikk@users.noreply.github.com>
2022-11-11 10:43:33 +0530

    Create spinner.component.ts

---
+++ src/app/components/spinner/spinner.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { SpinnerService } from './spinner.service';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss']
+})
+export class SpinnerComponent implements OnInit {
+
+  showSpinner = false;
+
+  constructor(private spinnerService: SpinnerService, private cdRef: ChangeDetectorRef) {
+
+  }
+
+  ngOnInit() {
+    this.init();
+  }
+
+  init() {
+
+    this.spinnerService.getSpinnerObserver().subscribe((status) => {
+      this.showSpinner = (status === 'start');
+      this.cdRef.detectChanges();
+    });
+  }
+
+}
